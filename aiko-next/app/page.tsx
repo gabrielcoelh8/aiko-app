4341b058acdb64cea4ba46111a20bf8fd4e900cd
gabrielcoelh8 <gabrielroberto099@gmail.com>
2024-09-27 00:40:05 -0400

    feat: add antd e cria layout base

--- aiko-next/app/page.tsx
+++ aiko-next/app/page.tsx
@@ -2,7 +2,7 @@ import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
-    <section className="h-screen bg-slate-400 font-bookerly">
+    <section className="">
       <Navbar />
     </section>
   );
