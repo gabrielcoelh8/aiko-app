14e660da38aaf3fbb349d33158eaf36ea5cc3b82
gabrielcoelh8 <gabrielroberto099@gmail.com>
2024-10-03 18:17:44 -0400

    feat: cria modelo de post doc: add propriedades da aplicacao

---
+++ aiko-api/app/src/main/java/app/aiko/model/Post.java
@@ -0,0 +1,13 @@
+package app.aiko.model;
+
+import java.util.List;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+public class Post {
+}
